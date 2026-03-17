78845149419575d50a6b8fa057f8a50946830ebb
Deepak Vankadaru <deepak@localhost>
2026-03-16 20:00:14 -0700

    ibkr: Added error code constants

---
+++ ibkr/internal/codes.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2026 Deepak Vankadaru
+
+package internal
+
+// HTTP status codes returned by the IBKR Client Portal API gateway.
+const (
+	StatusUnauthorized    = 401
+	StatusNotFound        = 404
+	StatusTooManyRequests = 429
+)
